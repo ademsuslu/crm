3721973537b48a5f4279acf3b4fcfd26ebf2f5f7
ademsuslu <ademsuslu9080@gmail.com>
2024-11-03 16:40:59 +0300

    refactor:content added solitions

--- components/shared/solitions.tsx
+++ components/shared/solitions.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 
 const Solitions = () => {
-  return (
-    <div className='flex flex-col justify-center w-full'>
+    return (
+        <div className='flex flex-col justify-center w-full'>
 
-        <div className='w-[1000px] h-[500px] mx-auto '>
-            <img src="/assets/crms.jpeg" className='w-full h-full object-cover rounded' alt="" />
+            <div className='w-[1000px] h-[500px] mx-auto '>
+                <img src="/assets/crms.jpeg" className='w-full h-full object-cover rounded' alt="" />
+            </div>
+            <div className='mt-2'>
+                <h1>
+                    Transform Customer Relationships with a Unified CRM Platform
+                </h1>
+
+                <div>
+                    <p>A unified CRM platform empowers businesses to build stronger, more meaningful relationships with their customers. By consolidating data across marketing, sales, and support teams, a CRM ensures everyone has access to the same insights, streamlining workflows and enhancing customer experiences. With a CRM, companies can track every interaction, anticipate customer needs, and tailor communications effectively. Built-in analytics and automation further drive efficiency, enabling teams to prioritize leads, personalize outreach, and close deals faster. Whether nurturing new prospects or supporting loyal customers, a CRM provides the tools to maximize each customer journey and foster long-term growth.</p>
+                </div>
+            </div>
+
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Solitions
