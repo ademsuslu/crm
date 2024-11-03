f4405f3818a17d57b4ae8b1613bc4c7d84dd004d
ademsuslu <ademsuslu9080@gmail.com>
2024-11-03 17:06:41 +0300

    feat:products component complated

--- components/shared/products.tsx
+++ components/shared/products.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { projects } from "@/data/projects";
+import { HoverEffect } from "../ui/card-hover-effect";
 
-const Products = () => {
-  return (
-    <div>Products</div>
-  )
+ const Products =() =>{
+  
+    return (
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={projects} />
+    </div>
+  );
 }
 
 export default Products
