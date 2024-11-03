4d01ded05e24b451c1761c9400099eb377e563ce
ademsuslu <ademsuslu9080@gmail.com>
2024-11-03 15:00:03 +0300

    feat:delete globe

--- app/(root)/mission/page.tsx
+++ app/(root)/mission/page.tsx
@@ -1,12 +1,12 @@
 import { Globe } from "@/components/shared/globe"
-import {sampleArcs} from "@/data/sampleArcs"
 
 import React from 'react'
+
+
 
 const MissionPage = () => {
   return (
     <div>
-       <Globe data={sampleArcs}  />
     </div>
   )
 }
