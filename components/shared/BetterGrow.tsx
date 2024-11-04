1e0aad28fdef71bdb75db3c1f45ad9f42d51fd63
ademsuslu <ademsuslu9080@gmail.com>
2024-11-04 16:27:58 +0300

    refactot:create better grow content and ımage

--- components/shared/BetterGrow.tsx
+++ components/shared/BetterGrow.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 const BetterGrow = () => {
   return (
-    <div className='flex flex-col md:flex-row justify-between items-center'>
-            <div>
-                text
+    <div className='flex flex-col md:flex-row justify-around items-center'>
+            <div className='flex flex-col'>
+             <h1 className='font-mono text-2xl my-2'>Grow better with HubSpot today</h1>
+             <div className='flex items-center justify-center space-x-3'>
+                <Button asChild variant={"secondary"} className="rounded font-mono mt-3">
+                    <Link href="/sign-in">Get a demo</Link>
+                </Button>
+                <Button asChild variant={"secondary"} className="rounded font-mono mt-3">
+                    <Link href="/sign-in">Get Started free</Link>
+                </Button>
+            </div>
             </div>
             <div>
-                img
+            <img
+                  className="rounded"
+                  src="/assets/bettergrow.jpeg"
+                  alt=""
+                  width="550"
+                  height="300"
+                  loading="lazy"
+                />
             </div>
     </div>
   )
