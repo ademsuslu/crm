5f60106eb3851100d92bb3c9499878ca82c21ad8
ademsuslu <ademsuslu9080@gmail.com>
2024-11-04 16:53:20 +0300

    feat:foter comp complated

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
             <hr />
             <section className="mt-4 flex flex-col justify-center items-center w-full">
-                <div className="flex flex-col md:flex-row justify-around items-center w-full">
+                <div className="flex flex-col md:flex-row justify-around items-center w-full space-y-3">
                     <Link className="ga_nav_link hsg-footer__app" href="/" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Download on the App Store">
                         <img className="homepage-mobile-footer-apple" src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/app%20store%20high%20res.png?width=136&amp;height=45&amp;name=app%20store%20high%20res.png" alt="Download on the App Store" width="136" height="45" loading="lazy" sizes="(max-width: 136px) 100vw, 136px" />
                     </Link>
@@ -20,35 +20,31 @@ const Footer = () => {
                     <p className='font-mono text-muted'>Copyright © 2024 CRM, Inc.</p>
                 </div>
                 <div className="hsg-footer__copyright">
-                    <ul className='flex  space-x-6'>
-                        <li>
-                            <Link href="/" className="underline" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
-                                Legal Stuff
-                            </Link>
-                        </li>
-
-
-
-
-                        <li>
-                            <Link href="/" className="underline" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
-                                Privacy Policy
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/" className="underline" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
-                                Security
-
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/" className="underline" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
+                    <ul className='flex flex-col md:flex-row  md:space-x-6'>
+                        <li className='p-0 m-0'>
+                            <Link href="/" className="underline w-full m-0 p-0" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
                                 Website Accessibility
 
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="underline" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
+                        <li className='p-0 m-0'>
+                            <Link href="/" className="underline w-full m-0 p-0" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
+                                Legal Stuff
+                            </Link>
+                        </li>
+                        <li className='p-0 m-0'>
+                            <Link href="/" className="underline w-full m-0 p-0" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
+                                Privacy Policy
+                            </Link>
+                        </li>
+                        <li className='p-0 m-0'>
+                            <Link href="/" className="underline w-full m-0 p-0" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
+                                Security
+
+                            </Link>
+                        </li>
+                        <li className='p-0 m-0'>
+                            <Link href="/" className="underline w-full m-0 p-0" data-ga_nav_type="footer_nav" data-ga_nav_tree_text="Legal Stuff">
                                 Manage Cookies
 
                             </Link>
