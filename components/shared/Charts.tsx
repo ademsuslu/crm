9398ac6d8e8408579707343676864aadbaf92012
ademsuslu <ademsuslu9080@gmail.com>
2024-11-04 19:29:11 +0300

    refactorpiechart added

--- components/shared/Charts.tsx
+++ components/shared/Charts.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { LineCharts } from './LineChart'
 import { SpiderChart } from './SpiderChart'
+import { PieCharts } from './PieCharts'
 
 const Charts = () => {
   return (
     <div className='flex flex-col md:flex-row justify-around w-full '>
-        <LineCharts/>
         <SpiderChart/>
+        <LineCharts/>
+        <PieCharts/>
     </div>
   )
 }
