ca62b73b32b68107e1250736a735e1b390912020
ademsuslu <ademsuslu9080@gmail.com>
2024-11-04 19:21:36 +0300

    refactor:chart content

--- components/shared/LineChart.tsx
+++ components/shared/LineChart.tsx
@@ -22,11 +22,11 @@ export const description = "A line chart with a label"
 
 const chartData = [
   { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
+  { month: "February", desktop: 214, mobile: 200 },
   { month: "March", desktop: 237, mobile: 120 },
   { month: "April", desktop: 73, mobile: 190 },
   { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
+  { month: "June", desktop: 305, mobile: 200 },
 ]
 
 const chartConfig = {
