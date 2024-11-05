954e5c322508784e092c9f90ccd65d9bd1d43d2e
ademsuslu <ademsuslu9080@gmail.com>
2024-11-05 13:49:11 +0300

    refactor:h1 content

--- components/shared/prices.tsx
+++ components/shared/prices.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Prices = () => {
   return (
-    <div>Prices</div>
+    <div className='flex flex-col w-full'>
+        <div className='flex flex-col'>
+            <h1 className='text-2xl font-mono text-center'>
+            Marketing Software That Grows With You
+            </h1>
+        </div>
+        <div></div>
+    </div>
   )
 }
 
