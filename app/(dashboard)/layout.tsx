407b4791da017a5218d2209db5c086d66444e638
ademsuslu <ademsuslu9080@gmail.com>
2024-11-06 19:49:03 +0300

    refactor: ı was env change railway added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider >
       <AppSidebar />
-      <main className="ps-2 w-full">
+      <main className="px-2 w-full">
         <SidebarTrigger />
         {children}
       </main>
