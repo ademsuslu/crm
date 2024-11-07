be8e5187b3f233e343e1b3345779192e63c3d228
ademsuslu <ademsuslu9080@gmail.com>
2024-11-07 17:10:06 +0300

    refactor:company information

--- app/(dashboard)/customer/[id]/page.tsx
+++ app/(dashboard)/customer/[id]/page.tsx
@@ -25,7 +25,9 @@ export default async function CustomerDetails({
         <TabsList>
           <TabsTrigger value="Personal">Personal</TabsTrigger>
           <TabsTrigger value="contact_information">Contact information</TabsTrigger>
+          <TabsTrigger value="company_information">Company information</TabsTrigger>
         </TabsList>
+       
         <TabsContent  className="flex gap-2 " value="Personal">
           <div className="flex flex-col gap-2">
             <div className="flex gap-2">
@@ -47,26 +49,68 @@ export default async function CustomerDetails({
           </div>
         </TabsContent>
         <TabsContent value="contact_information">
-       <div className="flex flex-col gap-2">
-            <div className="flex gap-2">
-                <label>Phone: </label>
-                <Link target="_blank" className="underline" href={`tel:${data.iletisim_bilgileri.telefon}`}>{data.iletisim_bilgileri.telefon}</Link>
-            </div>
-            <div className="flex gap-2">
-                <label>Email: </label>
-                <Link target="_blank" className="underline" href={`mailto:${data.iletisim_bilgileri.email}`}>{data.iletisim_bilgileri.email}</Link> 
-            </div>
-            <div className="flex gap-2">
-                <label>Twitter: </label>
-                <Link target="_blank" className="underline" href={`https://x.com/${data.iletisim_bilgileri.sosyal_medya.twitter}`}>{data.iletisim_bilgileri.sosyal_medya.twitter}</Link> 
-        
-            </div>
-            <div className="flex gap-2">
-                <label>Linkedin: </label>
-                <Link target="_blank" className="underline" href={`https://${data.iletisim_bilgileri.sosyal_medya.linkedin}`}>{data.iletisim_bilgileri.sosyal_medya.linkedin}</Link> 
-            </div>
+        <div className="flex flex-col gap-2">
+              <div className="flex gap-2">
+                  <label>Phone: </label>
+                  <Link target="_blank" className="underline" href={`tel:${data.iletisim_bilgileri.telefon}`}>{data.iletisim_bilgileri.telefon}</Link>
+              </div>
+              <div className="flex gap-2">
+                  <label>Email: </label>
+                  <Link target="_blank" className="underline" href={`mailto:${data.iletisim_bilgileri.email}`}>{data.iletisim_bilgileri.email}</Link> 
+              </div>
+              <div className="flex gap-2">
+                  <label>Twitter: </label>
+                  <Link target="_blank" className="underline" href={`https://x.com/${data.iletisim_bilgileri.sosyal_medya.twitter}`}>{data.iletisim_bilgileri.sosyal_medya.twitter}</Link> 
+          
+              </div>
+              <div className="flex gap-2">
+                  <label>Linkedin: </label>
+                  <Link target="_blank" className="underline" href={`https://${data.iletisim_bilgileri.sosyal_medya.linkedin}`}>{data.iletisim_bilgileri.sosyal_medya.linkedin}</Link> 
+              </div>
 
-          </div>
+            </div>
+        </TabsContent>
+        <TabsContent value="company_information">
+           {/* 
+         "sirket_bilgileri": {
+      "sirket_adresi": {
+        "sokak": "Danışmanlık Sok. No:12",
+        "sehir": "İzmir",
+        "posta_kodu": "35100",
+        "ulke": "Türkiye"
+      },
+      "sirket_adi": "XYZ Danışmanlık A.Ş.",
+      "gorev": "Pazarlama Müdürü"
+    },
+
+        */}
+        <div className="flex flex-col gap-2">
+              <div className="flex gap-2">
+                  <label>Company Name: </label>
+                 <span>{data.sirket_bilgileri.sirket_adi}</span>
+              </div>
+              <div className="flex gap-2">
+                  <label>Company Mission: </label>
+                 <span>{data.sirket_bilgileri.gorev}</span>
+              </div>
+              <div className="flex gap-2">
+                  <label>Company Address: </label>
+                 <span>{data.sirket_bilgileri.sirket_adresi.sokak}</span>
+              </div>
+              <div className="flex gap-2">
+                  <label>Company City: </label>
+                 <span>{data.sirket_bilgileri.sirket_adresi.sehir}</span>
+              </div>
+              <div className="flex gap-2">
+                  <label>Company Zipcode: </label>
+                 <span>{data.sirket_bilgileri.sirket_adresi.posta_kodu}</span>
+              </div>
+              <div className="flex gap-2">
+                  <label>Company Country: </label>
+                 <span>{data.sirket_bilgileri.sirket_adresi.ulke}</span>
+              </div>
+
+            </div>
         </TabsContent>
       </Tabs>
 
