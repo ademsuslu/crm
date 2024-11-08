33be5a4905c6742f05c3fb1dc64914e8fcad6211
ademsuslu <ademsuslu9080@gmail.com>
2024-11-09 02:16:05 +0300

    refactor:hero font changed

--- components/shared/hero.tsx
+++ components/shared/hero.tsx
@@ -1,9 +1,8 @@
-
 "use client"
 
 import * as React from "react"
+import { Roboto } from "next/font/google"
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
-
 import {
     Card,
     CardContent,
@@ -21,7 +20,13 @@ import {
 import { Button } from "../ui/button"
 import Link from "next/link"
 import { TrendingUp } from "lucide-react"
+
 export const description = "A multiple bar chart"
+
+const roboto = Roboto({
+    weight: ['400', '700'],
+    subsets: [ 'greek',],
+})
 
 const Hero = () => {
     const chartData = [
@@ -43,26 +48,26 @@ const Hero = () => {
         },
     } satisfies ChartConfig
 
-
     return (
-        <div className="flex  mt-16 flex-col md:flex-row  items-center justify-between">
-            <div className="flex z-30 flex-col items-start justify-start">
-                <h1 className="text-3xl  mb-2 text-white font-mono uppercase">
+        <div className={`flex flex-col md:flex-row items-center justify-between mt-16 ${roboto.className}`}>
+            <div className="flex flex-col items-start z-30">
+                <h1 className="text-3xl text-white uppercase mb-2">
                     Shape the Future with Your Data!
                 </h1>
-                <span className="my-1 text-white font-mono leading-normal">
-                    Analyze your data to optimize your business processes and strengthen your strategic decisions. <br />
+                <span className={`text-white tracking-wide  my-1  ${roboto.className}`} >
+                    Analyze your data to optimize your business processes and strengthen your strategic decisions.
+                    <br />
                     Discover growth opportunities with meaningful charts and reports.
-                    </span>
-                <Button asChild  size={"lg"} className="rounded font-mono mt-3">
-                    <Link href="/sign-in" className="text-lg" >Get Started</Link>
+                </span>
+                <Button asChild size="lg" className="text-lg rounded mt-3">
+                    <Link href="/sign-in">Get Started</Link>
                 </Button>
             </div>
-            <div className="flex  z-30 items-center my-3 md:my-0 justify-between w-full md:w-96">
+            <div className="flex items-center justify-between w-full md:w-96 z-30 my-3 md:my-0">
                 <Card className="w-full">
                     <CardHeader>
-                        <CardTitle className="font-mono">Bar Chart - Multiple</CardTitle>
-                        <CardDescription className="font-mono">January - June 2024</CardDescription>
+                        <CardTitle>Bar Chart - Multiple</CardTitle>
+                        <CardDescription>January - June 2024</CardDescription>
                     </CardHeader>
                     <CardContent>
                         <ChartContainer config={chartConfig}>
@@ -84,11 +89,11 @@ const Hero = () => {
                             </BarChart>
                         </ChartContainer>
                     </CardContent>
-                    <CardFooter className="flex-col items-start gap-2 text-sm">
-                        <div className="flex gap-2 font-mono leading-none">
+                    <CardFooter className="flex flex-col items-start gap-2 text-sm">
+                        <div className="flex gap-2 leading-none">
                             Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
                         </div>
-                        <div className="leading-none text-muted-foreground font-mono">
+                        <div className="text-muted-foreground leading-none">
                             Showing total visitors for the last 6 months
                         </div>
                     </CardFooter>
@@ -97,7 +102,5 @@ const Hero = () => {
         </div>
     )
 }
-
-
 
 export default Hero
