da4b3405ee99b9c5704bec2c4c52da556ca7ad24
ademsuslu <ademsuslu9080@gmail.com>
2024-11-08 17:50:14 +0300

    refactor:trigger

--- app/(dashboard)/customer/[id]/page.tsx
+++ app/(dashboard)/customer/[id]/page.tsx
@@ -21,14 +21,15 @@ export default async function CustomerDetails({
     </div>
     <div className="w-full">
       <Tabs defaultValue="Personal" className="">
-        <TabsList className="items-start justify-start rounded-md bg-muted p-1 text-muted-foreground grid h-auto min-h-10 w-full grid-cols-1 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7">
-          <TabsTrigger className="" value="Personal">Personal</TabsTrigger>
-          <TabsTrigger className="" value="contact_information">Contact information</TabsTrigger>
-          <TabsTrigger className="" value="company_information">Company information</TabsTrigger>
-          <TabsTrigger className="" value="segmentasyon">Segmentasyon</TabsTrigger>
-          <TabsTrigger className="" value="iliskiler">Relations</TabsTrigger>
-          <TabsTrigger className="" value="pazarlama_izinleri">Marketing Permissions</TabsTrigger>
-          <TabsTrigger className="" value="order_details">Order Details</TabsTrigger>
+        <TabsList
+         className="items-start justify-start rounded-md bg-muted p-1 gap-3 text-muted-foreground grid h-auto min-h-10 w-full grid-cols-1 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-5">
+          <TabsTrigger className="text-sm font-mono p-0 w-full" value="Personal">Personal</TabsTrigger>
+          <TabsTrigger className="text-sm font-mono p-0 w-full" value="contact_information">Contact information</TabsTrigger>
+          <TabsTrigger className="text-sm font-mono p-0 w-full" value="company_information">Company information</TabsTrigger>
+          <TabsTrigger className="text-sm font-mono p-0 w-full" value="segmentasyon">Segmentasyon</TabsTrigger>
+          <TabsTrigger className="text-sm font-mono p-0 w-full" value="iliskiler">Relations</TabsTrigger>
+          <TabsTrigger className="text-sm font-mono p-0 w-full" value="pazarlama_izinleri">Permissions</TabsTrigger>
+          <TabsTrigger className="text-sm font-mono p-0 w-full" value="order_details">Order</TabsTrigger>
         </TabsList>
         <TabsContent className="flex gap-2 " value="Personal">
           <div className="flex flex-col gap-2">
