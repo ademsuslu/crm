1e5c364f66bb89e3adae6de3f2bf4ab977557a76
ademsuslu <ademsuslu9080@gmail.com>
2024-11-10 00:54:42 +0300

    refactor:url

--- app/(dashboard)/customer/page.tsx
+++ app/(dashboard)/customer/page.tsx
@@ -3,12 +3,11 @@ import DataTableDemo  from "@/components/shared/DataTable"
 import SearchComponent from '@/components/shared/detay/search';
 
 export default async function CustomerPage(){
-    
-        const response = await fetch(`${process.env.NEXT_API_URL}/customers`,{ cache: 'no-store' });
+    const url = process.env.NEXT_API_URL || "http://localhost:5000/api/customers" 
+        const response = await fetch(`${url}/customers`,{ cache: 'no-store' });
         const data = await response.json();
 
     return <div className="w-full">
-        
        <Suspense fallback={<p>Loading feed...</p>}>
         <DataTableDemo data={data} />  
        </Suspense> 
