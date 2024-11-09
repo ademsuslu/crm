000917b9e706c0a7954e0d752fab52b25f3e04f6
ademsuslu <ademsuslu9080@gmail.com>
2024-11-09 14:12:01 +0300

    refactor:font-mono font deleting

--- components/shared/solitions.tsx
+++ components/shared/solitions.tsx
@@ -10,17 +10,17 @@ const Solitions = () => {
                 <img src="/assets/crms.jpeg" className='w-full h-full object-cover rounded' alt="" />
             </div>
             <div className='mt-6 '>
-                <h1 className='mb-3 text-2xl font-mono'>
+                <h1 className='mb-3 text-2xl '>
                     Transform Customer Relationships with a Unified CRM Platform
                 </h1>
                 <div>
-                    <p className='font-mono'>  A unified CRM platform empowers businesses to build stronger, more meaningful relationships with their customers. By consolidating data across marketing, sales, and support teams, a CRM ensures everyone has access to the same insights, streamlining workflows and enhancing customer experiences. With a CRM, companies can track every interaction, anticipate customer needs, and tailor communications effectively. Built-in analytics and automation further drive efficiency, enabling teams to prioritize leads, personalize outreach, and close deals faster. Whether nurturing new prospects or supporting loyal customers, a CRM provides the tools to maximize each customer journey and foster long-term growth.</p>
+                    <p className=''>  A unified CRM platform empowers businesses to build stronger, more meaningful relationships with their customers. By consolidating data across marketing, sales, and support teams, a CRM ensures everyone has access to the same insights, streamlining workflows and enhancing customer experiences. With a CRM, companies can track every interaction, anticipate customer needs, and tailor communications effectively. Built-in analytics and automation further drive efficiency, enabling teams to prioritize leads, personalize outreach, and close deals faster. Whether nurturing new prospects or supporting loyal customers, a CRM provides the tools to maximize each customer journey and foster long-term growth.</p>
                 </div>
                 <div className='flex items-center justify-start space-x-3 mt-2'>
-                <Button asChild variant={"outline"} size={"default"} className="rounded font-mono mt-3">
+                <Button asChild variant={"outline"} size={"default"} className="rounded  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get Started free</Link>
                 </Button>
-                <Button asChild size={"default"}  variant={"secondary"} className="rounded  border border-slate-400 px-10 font-mono mt-3">
+                <Button asChild size={"default"}  variant={"secondary"} className="rounded  border border-slate-400 px-10  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get a demo</Link>
                 </Button>
             </div>
--- components/shared/whatscrm.tsx
+++ components/shared/whatscrm.tsx
@@ -12,13 +12,13 @@ const Whatscrm = () => {
            <video src="/assets/crm.mp4" className="w-full h-full" controls ></video>
        </div>
         <div className='flex flex-col  h-[240px]'>
-            <h2 className='text-2xl font-mono'>What is Crm?</h2>
-            <span className='my-4 font-mono'>Customer relationship management is an approach to managing a company's interactions with existing and potential customers.</span>
+            <h2 className='text-2xl '>What is Crm?</h2>
+            <span className='my-4 '>Customer relationship management is an approach to managing a company's interactions with existing and potential customers.</span>
             <div className='flex items-center justify-start space-x-3'>
-                <Button asChild variant={"outline"} size={"default"} className="rounded font-mono mt-3">
+                <Button asChild variant={"outline"} size={"default"} className="rounded  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get Started free</Link>
                 </Button>
-                <Button asChild size={"default"}  variant={"secondary"} className="rounded  border border-slate-400 px-10 font-mono mt-3">
+                <Button asChild size={"default"}  variant={"secondary"} className="rounded  border border-slate-400 px-10  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get a demo</Link>
                 </Button>
             </div>
