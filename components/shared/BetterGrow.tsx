e17084eaea8b2c32a73f171234c5117246849e38
ademsuslu <ademsuslu9080@gmail.com>
2024-11-09 14:16:06 +0300

    refactor:better grow font-delete

--- components/shared/BetterGrow.tsx
+++ components/shared/BetterGrow.tsx
@@ -6,12 +6,12 @@ const BetterGrow = () => {
   return (
     <div className='flex flex-col md:flex-row justify-around items-center space-y-4'>
             <div className='flex flex-col'>
-             <h1 className='font-mono text-2xl my-2'>Grow better with Crm today</h1>
+             <h1 className=' text-2xl my-2'>Grow better with Crm today</h1>
              <div className='flex items-center justify-center space-x-3'>
-             <Button asChild variant={"outline"} size={"default"} className="rounded   border border-slate-400 font-mono mt-3">
+             <Button asChild variant={"outline"} size={"default"} className="rounded   border border-slate-400  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get Started free</Link>
                 </Button>
-                <Button asChild size={"default"}  variant={"secondary"} className="rounded border px-10 font-mono mt-3">
+                <Button asChild size={"default"}  variant={"secondary"} className="rounded border px-10  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get a demo</Link>
                 </Button>
             </div>
