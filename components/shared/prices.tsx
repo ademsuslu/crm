07ee7bbff54517e6f9971a9cab00003384e3dbd0
ademsuslu <ademsuslu9080@gmail.com>
2024-11-09 14:13:22 +0300

    refactor:prices font delete

--- components/shared/prices.tsx
+++ components/shared/prices.tsx
@@ -7,10 +7,10 @@ const Prices = () => {
     return (
         <div className='flex flex-col w-full'>
             <div className='flex flex-col'>
-                <h1 className='text-2xl font-mono text-start'>
+                <h1 className='text-2xl  text-start'>
                     Marketing Software That Grows With You
                 </h1>
-                <span className='text-start font-mono my-2'>
+                <span className='text-start  my-2'>
                     Start with free tools and pay as you grow, or hit the ground running with one of our premium editions.
                 </span>
             </div>
@@ -18,26 +18,26 @@ const Prices = () => {
             <div className='flex flex-col md:flex-row   mt-4 gap-3'>
                 <div className='flex  border flex-col w-full rounded-lg md:w-1/4 hover:scale-105 transition-all'>
                     <div className='flex flex-col items-start justify-start p-4'>
-                        <h2 className='text-2xl font-mono'>
+                        <h2 className='text-2xl '>
                             Free
                         </h2>
-                        <p className='font-mono text-foreground my-2 text-gray-300'>No credit card required</p>
-                        <span className='text-center font-mono'>
+                        <p className=' text-foreground my-2 text-gray-300'>No credit card required</p>
+                        <span className='text-center '>
                             $0/month
                         </span>
                         <div className="border w-full mt-4 border-slate-600 "></div>
                         <div className='flex flex-col  mt-6'>
 
-                            <h1 className='font-mono text-xl my-2'>Popular features</h1>
+                            <h1 className=' text-xl my-2'>Popular features</h1>
                             <ul className=' space-y-1'>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span> Email marketing </span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Forms</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Live chat</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Ad management </span>                       </li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /><span>Mobile Optimization</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span> Email marketing </span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Forms</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Live chat</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Ad management </span>                       </li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /><span>Mobile Optimization</span></li>
                             </ul>
                         </div>
-                        <Button  asChild className='rounded w-full font-mono mt-3'>
+                        <Button  asChild className='rounded w-full  mt-3'>
                             <Link href='/sign-in' className='text-lg font-bold'>Start</Link>
                         </Button>
                     </div>
@@ -45,26 +45,26 @@ const Prices = () => {
 
                 <div className='flex  border flex-col w-full rounded-lg md:w-1/4 hover:scale-105 transition-all'>
                     <div className='flex flex-col items-start justify-start p-4'>
-                        <h2 className='text-2xl font-mono'>
+                        <h2 className='text-2xl '>
                             Starter
                         </h2>
-                        <p className='font-mono text-foreground my-2 text-gray-300'>Starts at</p>
-                        <span className='text-center font-mono'>
+                        <p className=' text-foreground my-2 text-gray-300'>Starts at</p>
+                        <span className='text-center '>
                             $20/month
                         </span>
                         
                         <div className="border w-full mt-4 border-slate-600 "></div>
                         <div className='flex flex-col mt-6'>
-                            <h1 className='font-mono text-xl my-2'>Popular features</h1>
+                            <h1 className=' text-xl my-2'>Popular features</h1>
                             <ul className=' space-y-1'>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span> Everything in Free</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Multiple currencies</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Email health insights</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Calls-to-action</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /><span>No Crm branding </span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span> Everything in Free</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Multiple currencies</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Email health insights</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Calls-to-action</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /><span>No Crm branding </span></li>
                             </ul>
                         </div>
-                        <Button  asChild className='rounded w-full font-mono mt-3'>
+                        <Button  asChild className='rounded w-full  mt-3'>
                             <Link href='/sign-in' className='text-lg font-bold'>Start</Link>
                         </Button>
                     </div>
@@ -72,26 +72,26 @@ const Prices = () => {
              
                 <div className='flex  border flex-col w-full rounded-lg md:w-1/4 hover:scale-105 transition-all'>
                     <div className='flex flex-col items-start justify-start p-4'>
-                        <h2 className='text-2xl font-mono'>
+                        <h2 className='text-2xl '>
                             Professional
                         </h2>
-                        <p className='font-mono text-foreground my-2 text-gray-300'>Starts at</p>
-                        <span className='text-center font-mono'>
+                        <p className=' text-foreground my-2 text-gray-300'>Starts at</p>
+                        <span className='text-center '>
                         $890/month
                         </span>
                         
                         <div className="border w-full mt-4 border-slate-600 "></div>
                         <div className='flex flex-col mt-6'>
-                            <h1 className='font-mono text-xl my-2'>Popular features</h1>
+                            <h1 className=' text-xl my-2'>Popular features</h1>
                             <ul className=' space-y-1'>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span> Everything in Starter</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Campaign management</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>SEO</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Predictive lead scoring</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /><span>Custom reporting</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span> Everything in Starter</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Campaign management</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>SEO</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Predictive lead scoring</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /><span>Custom reporting</span></li>
                             </ul>
                         </div>
-                        <Button  asChild className='rounded w-full font-mono mt-3'>
+                        <Button  asChild className='rounded w-full  mt-3'>
                             <Link href='/sign-in' className='text-lg font-bold'>Start</Link>
                         </Button>
                     </div>
@@ -99,37 +99,37 @@ const Prices = () => {
              
                 <div className='flex  border flex-col w-full rounded-lg md:w-1/4 hover:scale-105 transition-all'>
                     <div className='flex flex-col items-start justify-start p-4'>
-                        <h2 className='text-2xl font-mono'>
+                        <h2 className='text-2xl '>
                         Enterprise
                         </h2>
-                        <p className='font-mono text-foreground my-2 text-gray-300'>Starts at</p>
-                        <span className='text-center font-mono'>
+                        <p className=' text-foreground my-2 text-gray-300'>Starts at</p>
+                        <span className='text-center '>
                         $3,600/month
                         </span>
                         
                         <div className="border w-full mt-4 border-slate-600 "></div>
                         <div className='flex flex-col mt-6'>
-                            <h1 className='font-mono text-xl my-2'>Popular features</h1>
+                            <h1 className=' text-xl my-2'>Popular features</h1>
                             <ul className=' space-y-1'>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Everything in Professional</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Adaptive testing</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Multuple revenue attribution</span></li>
-                                <li className='flex items-center font-mono'> <TiTick className='mr-2 text-xl' /> <span>Customer journey analytics</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Everything in Professional</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Adaptive testing</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Multuple revenue attribution</span></li>
+                                <li className='flex items-center '> <TiTick className='mr-2 text-xl' /> <span>Customer journey analytics</span></li>
                             </ul>
                         </div>
-                        <Button   asChild className='rounded w-full font-mono mt-4'>
+                        <Button   asChild className='rounded w-full  mt-4'>
                             <Link href='/sign-in' className='text-lg font-bold'>Start</Link>
                         </Button>
                     </div>
                 </div>
             </div>
-            <div className='mt-8 font-mono text-center  flex flex-col'>
+            <div className='mt-8  text-center  flex flex-col'>
                 <h1>It’s already easy to use. But we’re still here for you.</h1>
                 <div className='flex items-center justify-center space-x-3'>
-                <Button asChild variant={"outline"} size={"default"} className="rounded font-mono mt-3">
+                <Button asChild variant={"outline"} size={"default"} className="rounded  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get Started free</Link>
                 </Button>
-                <Button asChild size={"default"}  variant={"secondary"} className="rounded  border border-slate-400 px-10 font-mono mt-3">
+                <Button asChild size={"default"}  variant={"secondary"} className="rounded  border border-slate-400 px-10  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get a demo</Link>
                 </Button>
             </div>
