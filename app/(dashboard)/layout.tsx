6d7d80b81b32e58a9050fc1021e2feeb4e83ac4a
ademsuslu <ademsuslu9080@gmail.com>
2024-11-14 00:29:06 +0300

    refactor:dashboard for userbutton

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,16 @@
 import { AppSidebar } from "@/components/shared/app-sidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { UserButton } from "@clerk/nextjs"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider >
       <AppSidebar />
       <main className="px-2 w-full">
+        <div className="w-full p-1 flex justify-between items-center">
         <SidebarTrigger />
+        <UserButton />
+        </div>
         {children}
       </main>
     </SidebarProvider>
