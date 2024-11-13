88919a73f6806076331116e7a76c00f45ad47c39
ademsuslu <ademsuslu9080@gmail.com>
2024-11-13 23:53:23 +0300

    refactor:skeleton setting for table

--- components/shared/skeletons/table-skeleton.tsx
+++ components/shared/skeletons/table-skeleton.tsx
@@ -5,12 +5,13 @@ const TableSkeleton = () => {
     <div>
       <div className="gap-2">
         {/* Başlık için skeleton */}
-        <div  className="flex p-4 bg-gray-400">
-          <Skeleton className="h-[20px] mr-4 w-[20%]" />
-          <Skeleton className="h-[20px] mr-4 w-[20%]" />
-          <Skeleton className="h-[20px] mr-4 w-[20%]" />
-          <Skeleton className="h-[20px] mr-4 w-[20%]" />
+        <div  className="flex justify-between items-center p-4 bg-gray-700 rounded">
+          <Skeleton className="h-[20px] mr-4 w-[40%]" />
+
+          <div className="w-full flex items-end justify-end ">
+          <Skeleton className="h-[40px] mr-4 w-[40px]  rounded-full" />
           <Skeleton className="h-[20px] w-[20%]" />
+          </div>
         </div>
 
         {/* Satırların skeletonları */}
