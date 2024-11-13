cc6bc1d6f33be9aa336552689c90340c795e54c1
ademsuslu <ademsuslu9080@gmail.com>
2024-11-13 15:15:46 +0300

    refactor:robots and sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,14 @@
-import type { MetadataRoute } from 'next'
+// import type { MetadataRoute } from 'next'
 
-const URL = 'https://suslucrm.netlify.app/'
+// const URL = 'https://suslucrm.netlify.app/'
 
-export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/dashboard/',
-        },
-        sitemap: URL + '/sitemap.xml',
-    }
-}
+// export default function robots(): MetadataRoute.Robots {
+//     return {
+//         rules: {
+//             userAgent: '*',
+//             allow: '/',
+//             disallow: '/dashboard/',
+//         },
+//         sitemap: URL + '/sitemap.xml',
+//     }
+// }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,14 @@
 
-import type { MetadataRoute } from 'next'
+// import type { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-    const URL = 'https://suslucrm.netlify.app/'
-    return [
-        {
-            url: URL,
-            lastModified: new Date(),
-            changeFrequency: 'always',
-            priority: 1,
-        },
-    ]
-}
+// export default function sitemap(): MetadataRoute.Sitemap {
+//     const URL = 'https://suslucrm.netlify.app/'
+//     return [
+//         {
+//             url: URL,
+//             lastModified: new Date(),
+//             changeFrequency: 'always',
+//             priority: 1,
+//         },
+//     ]
+// }
