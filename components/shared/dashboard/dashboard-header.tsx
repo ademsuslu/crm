3e3439f91fbb50dce4d1fabf51ef9b53b358a60a
ademsuslu <ademsuslu9080@gmail.com>
2024-11-20 02:21:53 +0300

    feat:bradcrup complated

--- components/shared/dashboard/dashboard-header.tsx
+++ components/shared/dashboard/dashboard-header.tsx
@@ -6,50 +6,62 @@ import {
     BreadcrumbList,
     BreadcrumbPage,
     BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb"
-  import { Separator } from "@/components/ui/separator"
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Fragment } from "react"
 
 const DashboardHeader = () => {
-const path = usePathname()
-const pathNames = path.split("/").filter(path => path)
+    const path = usePathname()
+    const pathNames = path.split("/").filter(path => path)
 
 
-  return (
-    <div>
-          <header className="flex  top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-               {
-                pathNames.map((link, index) =>{
-                    const hrefs=`/${pathNames.slice(0,index + 1).join('/')}`;
-                    const linkName = link[0].toLocaleUpperCase() + link.slice(1,link.length) 
-                    const isLastPath = pathNames.length === index + 1;
-                    return (
-                       <Fragment key={index}>
+    return (
+        <div>
+            <header className="flex  top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        {
+                            pathNames.map((link, index) => {
+                                const hrefs = `/${pathNames.slice(0, index + 1).join('/')}`;
+                                const linkName = link[0].toLocaleUpperCase() + link.slice(1, link.length)
+                                const isLastPath = pathNames.length === index + 1;
+                                return (
+                                    <Fragment key={index}>
+                                        <BreadcrumbItem className="hidden md:block">
+                                            {
+                                                isLastPath ?
+                                                    <BreadcrumbLink asChild>
+                                                        <Link href={hrefs}>
+                                                            {linkName}
+                                                        </Link>
+                                                    </BreadcrumbLink> : <BreadcrumbPage>
+                                                    <Link href={hrefs}>
+                                                            {linkName}
+                                                        </Link>
+                                                    </BreadcrumbPage>
+                                            }
+                                        </BreadcrumbItem>
+                                        {pathNames.length !== index + 1 &&   <BreadcrumbSeparator  />}
+                                    </Fragment>
+                                )
 
-                       </Fragment>
-                    )
- 
-                })
-              
-            } 
-              </BreadcrumbItem>
-              {/* <BreadcrumbSeparator className="hidden md:block" />
+                            })
+
+                        }
+                        {/* <BreadcrumbSeparator className="hidden md:block" />
               <BreadcrumbItem>
                 <BreadcrumbPage>Data Fetching</BreadcrumbPage>
               </BreadcrumbItem>*/}
-            </BreadcrumbList> 
-          </Breadcrumb>
-        </header>
-    </div>
-  )
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </header>
+        </div>
+    )
 }
 
 export default DashboardHeader
