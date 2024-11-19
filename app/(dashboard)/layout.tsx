10e918bce4bbebe1c77480b1ee243676051682b0
ademsuslu <ademsuslu9080@gmail.com>
2024-11-20 01:44:03 +0300

    refactor:new dashboard-header

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,9 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+import DashboardHeader from "@/components/shared/dashboard/dashboard-header"
+
 import {
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from "@/components/ui/sidebar"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -19,23 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       {/* <Cards/> */}
       <SidebarInset>
-        <header className="flex  top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
+      <DashboardHeader/>
         <main className="flex flex-1 flex-col gap-4 p-4">
           {children}
         </main>
