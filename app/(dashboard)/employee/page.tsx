a9cad07091f488cdad6713266a0f7f196c952f7d
ademsuslu <ademsuslu9080@gmail.com>
2024-11-20 03:54:41 +0300

    refactor:middleware

--- app/(dashboard)/employee/page.tsx
+++ app/(dashboard)/employee/page.tsx
@@ -2,7 +2,7 @@
 export default async function Employee (){
     const response = await fetch(`${process.env.NEXT_API_URL}/employees`)
     const data = await response.json()
-    console.log(data)
+
     return <div>
         Employee
     </div>
