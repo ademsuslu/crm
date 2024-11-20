1fcd551d8f48ee9d237dca16347bace7917b8c7f
ademsuslu <ademsuslu9080@gmail.com>
2024-11-20 03:23:33 +0300

    refactor:footer for layout flex

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,12 +3,14 @@ import  Footer  from "@/components/shared/footer"
 
 const RootLayout = ({children}:{children:React.ReactNode}) => {
   return (
-    <div className=" container  mx-auto px-4">
+    <div className="container mx-auto px-4">
         <Navbar />
-        <main>{children}</main>
-        <div className="mt-6"></div>
-        <Footer/>
-      <div className="mt-3"></div>
+        <main className="flex flex-col  min-h-[calc(100vh-10.5rem-1px)]">
+        <div className="flex-1 flex flex-col h-full">
+        {children}
+        </div>
+        </main>
+        <Footer />
     </div>
   )
 }
