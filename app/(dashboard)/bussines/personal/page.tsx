30cce3951b53d5747eee4a91d39a93fdfb454a57
ademsuslu <ademsuslu9080@gmail.com>
2024-11-21 00:46:23 +0300

    refactor:cache-no store

--- app/(dashboard)/bussines/personal/page.tsx
+++ app/(dashboard)/bussines/personal/page.tsx
@@ -1,9 +1,10 @@
 import DataTablePersonal from "@/components/shared/DataTablePersonal"
 
 export default async function Employee() {
-    const response = await fetch(`${'https://crm-backend-production-e80f.up.railway.app/api'}/employees`)
+    const response = await fetch(`${'https://crm-backend-production-e80f.up.railway.app/api'}/employees`,{
+        cache:"no-store"
+    })
     const data = await response.json()
-    console.log("Employes", data)
     return <div>
         <DataTablePersonal data={data}/>
     </div>
