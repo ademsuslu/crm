7cb198d417a7fa855f0cabdacf8db0d9b2044376
ademsuslu <ademsuslu9080@gmail.com>
2024-11-21 02:36:10 +0300

    refactor:not-foundpage back button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,14 @@
-import { buttonVariants } from '@/components/ui/button'
+"use client"
+import { Button, buttonVariants } from '@/components/ui/button'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 export default function NotFound() {
+  const router = useRouter();
+  const handleGoBack = () => {
+    router.back(); // Bir önceki sayfaya döner
+  };
   return (
     <div className='flex flex-col w-full min-h-screen space-y-6 items-center justify-center'>
       <h1 className='text-xl font-bold'>404 - Not Found</h1>
@@ -11,7 +17,12 @@ export default function NotFound() {
         <svg className="animate-bounce w-6 h-6 text-white" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
           <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
         </svg>
-        <Link className={buttonVariants({ className: "capitalize" , variant:"secondary" })} href="/">back home</Link>
+        <Button
+      className={buttonVariants({ className: 'capitalize', variant: 'secondary' })}
+      onClick={handleGoBack}
+    >
+      Back Home
+    </Button>
       </div>
     </div>
   )
