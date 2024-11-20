deca235c031377dea3e393758f8bd98b737321fb
ademsuslu <ademsuslu9080@gmail.com>
2024-11-21 02:50:02 +0300

    refactor:Personal table actions complated ffor redirect

--- components/shared/DataTablePersonal.tsx
+++ components/shared/DataTablePersonal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { FaTasks } from "react-icons/fa";
+import { FaEdit, FaEye, FaTasks } from "react-icons/fa";
 import { FaPersonCirclePlus} from "react-icons/fa6";
 import * as React from "react"
 import {
@@ -106,22 +106,16 @@ export const columns: ColumnDef<Personal>[] = [
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          {/* <DropdownMenuItem >
-            <Link className="inline-flex items-center" href={`/customer/${row.original._id}`}>
+           <DropdownMenuItem >
+            <Link className="inline-flex items-center" href={`/personal/${row.original._id}`}>
               <FaEye className="w-4 h-4 mr-2" /> Show  Details
             </Link>
           </DropdownMenuItem>
           <DropdownMenuItem >
-            <Link className="inline-flex items-center" href={`/customer/edit/${row.original._id}`}>
+            <Link className="inline-flex items-center" href={`/personal/edit/${row.original._id}`}>
               <FaEdit className="w-4 h-4 mr-2" /> Edit
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem >
-            <Link className="inline-flex items-center" href={`/customer/${row.original._id}/create`}>
-              <IoIosTimer  className="w-5 h-5 mr-2" /> Add Reminder
-            </Link> 
-          </DropdownMenuItem>
-            */}
         </DropdownMenuContent>
       </DropdownMenu>
     }
