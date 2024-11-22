c68dd61f750171d7a45a1ff6539807058c736cf3
ademsuslu <ademsuslu9080@gmail.com>
2024-11-22 19:09:55 +0300

    refactor:personal detiils tab contents

--- app/(dashboard)/bussines/personal/[id]/page.tsx
+++ app/(dashboard)/bussines/personal/[id]/page.tsx
@@ -28,9 +28,9 @@ return <div className="flex flex-col justify-between items-center  gap-2">
       <div className="w-full">
       <Tabs defaultValue="Personal" className="">
         <TabsList
-          className="items-start justify-start rounded-md bg-muted p-1 gap-3 text-muted-foreground grid h-auto min-h-5 w-full grid-cols-1 sm:grid-cols-2">
+          className="items-start justify-start rounded-md bg-muted p-1 gap-3 text-muted-foreground grid h-auto min-h-5 w-[50%] grid-cols-1 sm:grid-cols-2">
           <TabsTrigger className="text-sm  p-0 w-full" value="Personal">Personal</TabsTrigger>
-           <TabsTrigger className="text-sm  p-0 w-full" value="busPersonal">Bussines</TabsTrigger> 
+           {/* <TabsTrigger className="text-sm  p-0 w-full" value="busPersonal">Bussines</TabsTrigger>  */}
 
         </TabsList>
         <TabsContent className="flex gap-2 " value="Personal">
@@ -51,9 +51,9 @@ return <div className="flex flex-col justify-between items-center  gap-2">
 
           </div>
         </TabsContent>
-         <TabsContent className="flex gap-2 " value="busPersonal">
+         {/* <TabsContent className="flex gap-2 " value="busPersonal">
            <DataTableBussines data={bussinesforpersonal} />
-        </TabsContent>
+        </TabsContent> */}
       </Tabs>
        
       </div>
