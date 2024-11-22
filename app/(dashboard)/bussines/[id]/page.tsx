b05146697ce6638473132fe4107b58b2f877912d
ademsuslu <ademsuslu9080@gmail.com>
2024-11-22 18:29:53 +0300

    refacto:bussines in personal for grid chaged

--- app/(dashboard)/bussines/[id]/page.tsx
+++ app/(dashboard)/bussines/[id]/page.tsx
@@ -6,26 +6,26 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import DataTableBusPersonal from "@/components/shared/datatables/bussines/personal/DataTableBusPersonal"
 
 export default async function BussinesDetails({
-    params,
-  }: {
-    params: Promise<{ id: string }>
-  }) {
-    const id = (await params).id
-    const response = await fetch(`${process.env.NEXT_API_URL}/businesses/${id}`,{ cache: 'no-store' })
-    const data = await response.json()
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const id = (await params).id
+  const response = await fetch(`${process.env.NEXT_API_URL}/businesses/${id}`, { cache: 'no-store' })
+  const data = await response.json()
   console.log(data)
-    return (<div className="flex flex-col justify-between items-center  gap-2">
-      <div className="flex w-full justify-between gap-2">
-       <Link href={"/bussines"} className={buttonVariants({})}>Back</Link>
-       <ButtonsExport type="bussines" id={id}/>
-     </div>
-     <div className="w-full">
+  return (<div className="flex flex-col justify-between items-center  gap-2">
+    <div className="flex w-full justify-between gap-2">
+      <Link href={"/bussines"} className={buttonVariants({})}>Back</Link>
+      <ButtonsExport type="bussines" id={id} />
+    </div>
+    <div className="w-full">
       <Tabs defaultValue="Busines" className="">
         <TabsList
-         className="items-start justify-start rounded-md bg-muted p-1 gap-3 text-muted-foreground grid h-auto min-h-10 w-full grid-cols-1 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-5">
+          className="items-start justify-start rounded-md bg-muted p-1 gap-3 text-muted-foreground grid h-auto min-h-5 w-full grid-cols-1 sm:grid-cols-2">
           <TabsTrigger className="text-sm  p-0 w-full" value="Busines">Busines</TabsTrigger>
           <TabsTrigger className="text-sm  p-0 w-full" value="busPersonal">Contact information</TabsTrigger>
-      
+
         </TabsList>
         <TabsContent className="flex gap-2 " value="Busines">
           <div className="flex flex-col gap-2">
@@ -37,21 +37,21 @@ export default async function BussinesDetails({
               <label>Address: </label>
               <span>{data.address}</span>
             </div>
-        
+
             <div className="flex gap-2">
               <label>Phone: </label>
               <span>{data.phone}</span>
             </div>
-        
+
           </div>
         </TabsContent>
         <TabsContent className="flex gap-2 " value="busPersonal">
-          <DataTableBusPersonal data={data?.employees}/>
+          <DataTableBusPersonal data={data?.employees} />
         </TabsContent>
       </Tabs>
 
     </div>
 
- 
-   </div>)
-  }
+
+  </div>)
+}
