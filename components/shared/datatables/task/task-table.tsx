6237b3ca9e68edb249b93c766f344f96c44b10ec
ademsuslu <ademsuslu9080@gmail.com>
2024-11-24 16:28:29 +0300

    refactor:details and edit direction

--- components/shared/datatables/task/task-table.tsx
+++ components/shared/datatables/task/task-table.tsx
@@ -122,12 +122,12 @@ export const columns: ColumnDef<Task>[] = [
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem >
-            <Link className="inline-flex items-center" href={`/bussines/${row.original._id}`}>
+            <Link className="inline-flex items-center" href={`/bussines/task/${row.original._id}`}>
               <FaEye className="w-4 h-4 mr-2" /> Show  Details
             </Link>
           </DropdownMenuItem>
           <DropdownMenuItem >
-            <Link className="inline-flex items-center" href={`/bussines/edit/${row.original._id}`}>
+            <Link className="inline-flex items-center" href={`/bussines/task/edit/${row.original._id}`}>
               <FaEdit className="w-4 h-4 mr-2" /> Edit
             </Link>
           </DropdownMenuItem>
