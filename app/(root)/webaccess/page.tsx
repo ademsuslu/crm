6a8991922fedcbe579ef109a5ce1cfca38d4053b
ademsuslu <ademsuslu9080@gmail.com>
2024-11-28 01:22:11 +0300

    feat:webaccess page content add finish

--- app/(root)/webaccess/page.tsx
+++ app/(root)/webaccess/page.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function WebaccessPage() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col my-10 w-full gap-8 justify-center items-start'>
         <div className='flex flex-col gap-2'>
-            <h1>Website Accessibility Statement</h1>
+            <h1 className='text-2xl'>Website Accessibility Statement</h1>
             <span>Last Modified: November 28, 2024</span>
         </div>
         
         <div className='flex flex-col gap-2'>
             <p>SusluCrm is committed to digital inclusion principles by striving to meet or exceed the requirements for the</p>
             <p>(WCAG) 2.1, Level AA Conformance (WCAG 2.1 Level AA) for its public-facing websites. We are constantly working towards improving the accessibility of our public-facing websites to provide an inclusive experience to all users of our website, including but not limited to those with visual, hearing, cognitive and motor impairments.</p>
-           <p>If you encounter accessibility barriers on our public-facing websites let us know by emailing website-accessibility@hubspot.com. Please be sure to specify the webpage and assistive technology (if applicable), and we will make reasonable efforts to meet your needs.</p>    
+           <p>If you encounter accessibility barriers on our public-facing websites let us know by emailing website-accessibility@SusluCrm.com. Please be sure to specify the webpage and assistive technology (if applicable), and we will make reasonable efforts to meet your needs.</p>    
         </div>
 
     </div>
