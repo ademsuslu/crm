88b27ba3631d069dbe621e0dbae3cc39ec0c9ecd
ademsuslu <ademsuslu9080@gmail.com>
2024-11-28 02:51:32 +0300

    refactor:auth for add img

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 
 export default function AuthLayout({
   children,
@@ -5,8 +6,10 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="w-full h-screen flex items-center justify-center text-white">
-        {children}
+    <div className="w-full h-full	 flex items-center justify-between text-white">
+       <Image className="" fill  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{objectFit: "cover", width:"100%", height:"100%"}} src={"/assets/login_register.webp"} alt="Login_Register"  	loading="lazy"/>
+    
+       <div className="w-full h-full flex items-center justify-center text-white">{children}</div>
     </div>
 
   )
