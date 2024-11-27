ef277d323ee32bd44223f0345563763a65879fed
ademsuslu <ademsuslu9080@gmail.com>
2024-11-28 01:35:50 +0300

    feat:PrivacyPolicyPage complated

--- app/(root)/privacypolicy/page.tsx
+++ app/(root)/privacypolicy/page.tsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function PrivacyPolicyPage() {
   return (
-    <div>PrivacyPolicyPage</div>
+    <div className='flex flex-col my-10 w-full gap-8 justify-center items-start'>
+      <h1 className='text-2xl'>SusluCrm Privacy Policy    </h1>
+      <div className='flex flex-col gap-2'>
+        <p>Last Modified: September 18, 2024</p>
+        <p>SusluCrm is committed to protecting your privacy. This Privacy Policy applies to the personal data we collect and process in the course of our business. SusluCrm acts in different ways when processing data:</p>
+
+
+        <ul className='list-disc px-6'>
+          <li>SusluCrm acts as a controller of your personal data when you sign up for our products and services, visit our websites or interact with us through other channels (for example in person events). We use this data for a variety of purposes, including to provide our products and services to you, contact you about relevant content, and improve and promote our products and services. </li>
+          <li>SusluCrm acts as a processor when our customers use our products and services to collect and process personal data in connection with their sales, marketing, and other business activities. In this case, we act as a processor (or service provider) on behalf of our customers (who are controllers of personal data) under the SusluCrm Data Processing Agreement included in the Customer Terms of Service. </li>
+          <li>SusluCrm also acts as a controller when we collect and process personal data about individuals in their professional capacity, and make that information available to customers for their own sales, marketing, and other business activities. This includes personal data we may collect indirectly about you, such as from publicly available sources, our customers, or other third parties. For more information see the “What Information We Collect and Process” section.</li>
+          <p className='py-8'>For the purposes of this Privacy Policy "Personal Data" means any information relating to an identified or identifiable individual. This Privacy Policy describes your choices and rights with respect to your Personal Data, including your rights of access and correction.  </p>
+          <li>For information about how to exercise your privacy rights when SusluCrm acts as a controller of personal data, please see the “Your Privacy Rights and Choices” section.</li>
+          <li>
+            To opt out and delete your personal data from our commercial dataset of professionals, please fill out this form
+          </li>
+          <li>If you wish to exercise your privacy rights regarding Personal Data we process on behalf of a customer, please refer to the customer policies and contact the customer directly.</li>
+
+          <p className='py-8'>We update this Privacy Policy from time to time and encourage you to review this Privacy Policy periodically. We will post any Privacy Policy changes on this page and, if the changes are material, notify you directly (for example, by sending you an email notification). Terms not otherwise defined in this Privacy Policy have the meaning given to them in the SusluCrm</p>  </ul>
+      </div>
+    </div>
   )
 }
