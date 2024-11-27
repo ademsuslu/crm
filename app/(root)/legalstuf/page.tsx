da514b635fa9da4cd4572e92178dab55fa917618
ademsuslu <ademsuslu9080@gmail.com>
2024-11-28 01:25:24 +0300

    refactor:legal stuf page complated

--- app/(root)/legalstuf/page.tsx
+++ app/(root)/legalstuf/page.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function LegalstufPage() {
   return (
-    <div>LegalstufPage</div>
+    <div className='flex flex-col my-10 w-full gap-8 justify-center items-start'>
+        <h1 className='text-2xl'>Talk Legal To Me</h1>
+    <div className='flex flex-col gap-2'>
+  
+        <p>There are a few legal documents that are central to life at SusluCrm -- to us, our customers, our prospects, our partners, and just plain old users of our website. To make it easy to find the information you’re looking for, we’ve assembled them all here under one roof and provided you with a quick rundown below of what you’ll find in each one.</p>
+       <p>We might make versions of these legal documents available in languages other than English. If we do, the English version will govern our relationship -- the translated version is provided for convenience only and will not be interpreted to modify the English version.</p>    
+    </div>
+
+</div>
   )
 }
