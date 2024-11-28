c3f434ec32174ee010d82c2af35fc25de4521781
ademsuslu <ademsuslu9080@gmail.com>
2024-11-28 03:21:05 +0300

    refactor:solitions in button in add icons

--- components/shared/solitions.tsx
+++ components/shared/solitions.tsx
@@ -18,8 +18,8 @@ const Solitions = () => {
                     <p className=''>  A unified CRM platform empowers businesses to build stronger, more meaningful relationships with their customers. By consolidating data across marketing, sales, and support teams, a CRM ensures everyone has access to the same insights, streamlining workflows and enhancing customer experiences. With a CRM, companies can track every interaction, anticipate customer needs, and tailor communications effectively. Built-in analytics and automation further drive efficiency, enabling teams to prioritize leads, personalize outreach, and close deals faster. Whether nurturing new prospects or supporting loyal customers, a CRM provides the tools to maximize each customer journey and foster long-term growth.</p>
                 </div>
                 <div className='flex items-center justify-start space-x-3 mt-2'>
-                <Button asChild variant={"outline"} size={"default"} className="rounded  mt-3">
-                    <Link href="/sign-in" className='text-lg'>Get Started free</Link>
+                <Button asChild size="lg" className="text-lg rounded mt-3">
+                    <Link href="/sign-in">👉 Start Now</Link>
                 </Button>
                 <Button asChild size={"default"}  variant={"secondary"} className="rounded  border border-slate-400 px-10  mt-3">
                     <Link href="/sign-in" className='text-lg'>Get a demo</Link>
