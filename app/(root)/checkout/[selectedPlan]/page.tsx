27fa6ccb56f83dfe79dcec3cd515553f5218a682
ademsuslu <ademsuslu9080@gmail.com>
2024-12-02 23:26:55 +0300

    refactor:selected plandate component created

--- app/(root)/checkout/[selectedPlan]/page.tsx
+++ app/(root)/checkout/[selectedPlan]/page.tsx
@@ -1,13 +1,7 @@
+import BillingDetay from "@/components/shared/billing/billing-detay";
 import { BillingFormComponent } from "@/components/shared/forms/billing/billing-form";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+
 import { plans } from "@/data/projects"
-import { FaCheckCircle, FaCalendarAlt, FaDollarSign } from "react-icons/fa";
 
 export default async function BussinesDetails({
   params,
@@ -17,54 +11,13 @@ export default async function BussinesDetails({
   const selectedPlan = (await params).selectedPlan
   const planData = plans.find(pland => pland.plan === selectedPlan)
 
-  const dateObj = new Date()
-  const newDate = dateObj.toLocaleDateString('pt-PT')
-  // how to get newDate to 30day after
-  const newDateEnd = new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000).toLocaleDateString('pt-PT')
+ 
 
   return <div className=" w-full  gap-4 grid grid-cols-1 md:grid-cols-2 justify-between p-6">
         <div className="border p-6 border-gray-300 shadow-lg rounded-lg bg-gray-900">
             <BillingFormComponent/>
         </div>
-       <Card>
-        <Card className=" border border-gray-300 shadow-lg rounded-lg bg-gray-900 text-white">
-          <CardHeader className="p-6">
-            <CardTitle className="text-2xl font-bold flex items-center">
-              {planData?.plan} <FaCheckCircle className="ml-2 text-green-400" />
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="p-6">
-            <div className="mb-4">
-              <h3 className="text-lg font-semibold mb-2">Features:</h3>
-              <ul className="list-disc list-inside">
-                {planData?.features.map((item: string, index: number) => (
-                  <li key={index} className="text-gray-400">
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="flex items-center mb-2">
-              <FaCalendarAlt className="mr-2 text-blue-500" />
-              <p>
-                Start Date: <span className="text-gray-400">{newDate}</span>
-              </p>
-            </div>
-            <div className="flex items-center">
-              <FaCalendarAlt className="mr-2 text-red-500" />
-              <p>
-                End Date: <span className="text-gray-400">{newDateEnd}</span>
-              </p>
-            </div>
-          </CardContent>
-          <CardFooter className="p-6 border-t border-gray-700 flex justify-between items-center">
-            <p className="text-lg font-semibold flex items-center">
-              <FaDollarSign className="mr-2 text-yellow-500" />
-              {planData?.price === 0 ? "Free" : `${planData?.price}`}
-            </p>
-          </CardFooter>
-        </Card>
-      </Card>
+        <BillingDetay  planData={planData}/>
 
   </div>
 }
