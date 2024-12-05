822ce8ae995043ea2670102247f84dd96bf85475
ademsuslu <ademsuslu9080@gmail.com>
2024-12-05 19:28:16 +0300

    refactor:next config

--- components/shared/blog/blog-items.tsx
+++ components/shared/blog/blog-items.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 import { BsCalendarPlus } from "react-icons/bs";
@@ -19,7 +20,9 @@ const BlogItems: React.FC<Props> = ({ data }) => {
     return (
         <Link className='border border-white shadow-sm rounded-md p-1' href={`blog/${data.title}`}>
             <div className="aspect-[800/600] overflow-hidden rounded-md ">
-                <img src={data.img} alt={data.title} loading="lazy" width="800" height="600" decoding="async" data-nimg="1" />
+                <div className=''>
+                    <Image src={data.img} alt={data.title} loading="lazy" width={800} height={600} decoding="async" data-nimg="1" />
+                </div>
             </div>
             <h2 className="mt-1 line-clamp-2 text-balance font-medium capitalize leading-tight">{data.title}</h2>
             <p className="mt-1 flex items-center text-xs leading-none text-muted-foreground">
