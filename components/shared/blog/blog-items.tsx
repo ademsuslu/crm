55638b6574937395ff9a318d8c88d7d08de12be3
ademsuslu <ademsuslu9080@gmail.com>
2024-12-05 17:56:24 +0300

    refactor:blog ıtems for skeleton

--- components/shared/blog/blog-items.tsx
+++ components/shared/blog/blog-items.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 interface Props {
       data: {
@@ -14,7 +15,15 @@ interface Props {
 
 const BlogItems:React.FC<Props> = ({data}) => {
   return (
-    <div className='bg-red-500 rounded'>{data.title}</div>
+    <Link href="/blog/neden-michelin-lastiklerini-tercih-etmelisiniz">
+        <div className="aspect-[800/600] overflow-hidden rounded-md">
+            <img alt={data.title} loading="lazy" width="800" height="600" decoding="async" data-nimg="1"  />
+        </div>
+        <h2 className="mt-1 line-clamp-2 text-balance font-medium capitalize leading-tight">{data.title}</h2>
+        <p className="mt-1 flex items-center text-xs leading-none text-muted-foreground">
+         {data.createdAt}
+        </p>
+ </Link>
   )
 }
 
