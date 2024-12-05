495427baec748049726a45ee68dbd1d8b010a030
ademsuslu <ademsuslu9080@gmail.com>
2024-12-05 18:01:33 +0300

    refactor:blog ıtems for ımg skeleton

--- components/shared/blog/blog-items.tsx
+++ components/shared/blog/blog-items.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
 interface Props {
-      data: {
+    data: {
         _id: number;
         title: string;
         desc: string;
@@ -13,18 +13,18 @@ interface Props {
     }
 }
 
-const BlogItems:React.FC<Props> = ({data}) => {
-  return (
-    <Link href="/blog/neden-michelin-lastiklerini-tercih-etmelisiniz">
-        <div className="aspect-[800/600] overflow-hidden rounded-md">
-            <img alt={data.title} loading="lazy" width="800" height="600" decoding="async" data-nimg="1"  />
-        </div>
-        <h2 className="mt-1 line-clamp-2 text-balance font-medium capitalize leading-tight">{data.title}</h2>
-        <p className="mt-1 flex items-center text-xs leading-none text-muted-foreground">
-         {data.createdAt}
-        </p>
- </Link>
-  )
+const BlogItems: React.FC<Props> = ({ data }) => {
+    return (
+        <Link href="/blog/neden-michelin-lastiklerini-tercih-etmelisiniz">
+            <div className="aspect-[800/600] overflow-hidden rounded-md">
+                <img src={data.img} alt={data.title} loading="lazy" width="800" height="600" decoding="async" data-nimg="1" />
+            </div>
+            <h2 className="mt-1 line-clamp-2 text-balance font-medium capitalize leading-tight">{data.title}</h2>
+            <p className="mt-1 flex items-center text-xs leading-none text-muted-foreground">
+                {data.createdAt}
+            </p>
+        </Link>
+    )
 }
 
 export default BlogItems
