94bf0fe33aef0568ef79e17e84b8d23d889c72b5
ademsuslu <ademsuslu9080@gmail.com>
2024-12-06 16:08:11 +0300

    refactor:blog items in button

--- components/shared/blog/blog-items.tsx
+++ components/shared/blog/blog-items.tsx
@@ -45,7 +45,7 @@ const BlogItems: React.FC<Props> = ({ data }) => {
                 </div>
                 <Link className={buttonVariants({
                     variant:"outline",
-                    className:"underline z-20 font-bold  bg-sky-800 hover:bg-sky-800",
+                    className:"underline z-20 font-bold border-white bg-sky-800 hover:bg-sky-800",
                 })} href={`/blog/${data.title}`}>
                     Read More
                 </Link>
